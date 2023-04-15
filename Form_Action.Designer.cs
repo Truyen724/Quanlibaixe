6c3f9133f320468f5118488c9a941d876a71667c
ThqnhNgqn <90490476+thqnhngqn-dev@users.noreply.github.com>
2023-04-15 21:20:57 +0700

    Tạo folder custom toolbox, folder UI-UX và folder screen shost

--- Form_Action.Designer.cs
+++ Form_Action.Designer.cs
@@ -41,36 +41,40 @@ namespace Quanlibaixe
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(41, 42);
+            this.label1.Location = new System.Drawing.Point(31, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(131, 39);
+            this.label1.Size = new System.Drawing.Size(104, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Biển số";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(225, 39);
+            this.textBox1.Location = new System.Drawing.Point(169, 32);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(203, 45);
+            this.textBox1.Size = new System.Drawing.Size(153, 37);
             this.textBox1.TabIndex = 1;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(48, 103);
+            this.dataGridView1.Location = new System.Drawing.Point(36, 84);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(786, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(590, 122);
             this.dataGridView1.TabIndex = 2;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.button1.Location = new System.Drawing.Point(206, 303);
+            this.button1.Location = new System.Drawing.Point(154, 246);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 59);
+            this.button1.Size = new System.Drawing.Size(94, 48);
             this.button1.TabIndex = 3;
             this.button1.Text = "Từ Chối";
             this.button1.UseVisualStyleBackColor = false;
@@ -79,9 +83,10 @@ namespace Quanlibaixe
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.button2.Location = new System.Drawing.Point(604, 303);
+            this.button2.Location = new System.Drawing.Point(453, 246);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(125, 59);
+            this.button2.Size = new System.Drawing.Size(94, 48);
             this.button2.TabIndex = 4;
             this.button2.Text = "Chấp nhận";
             this.button2.UseVisualStyleBackColor = false;
@@ -89,14 +94,15 @@ namespace Quanlibaixe
             // 
             // Form_Action
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(863, 434);
+            this.ClientSize = new System.Drawing.Size(647, 353);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form_Action";
             this.Text = "Form_Action";
             this.Load += new System.EventHandler(this.Form_Action_Load);
