123a47179dc64e713ba00ebb36aceef425602e3d
thqnhngqn-dev <thqnhngqn.dev@gmail.com>
2023-05-26 00:26:27 +0700

    update

--- infor.cs
+++ infor.cs
@@ -8,8 +8,8 @@ namespace Quanlibaixe
 {
     class infor
     {
-        public static string ConectionString = "server = DESKTOP-O41267U; database = Detect_bienso; integrated security = true";
-       // public static string ConectionString = "server = THQNHNGQN\\THQNHNGQN; database = Detect_bienso; integrated security = true";
+        //public static string ConectionString = "server = DESKTOP-O41267U; database = Detect_bienso; integrated security = true";
+        public static string ConectionString = "server = THQNHNGQN\\THQNHNGQN; database = Detect_bienso; integrated security = true";
         public static string id_access = "-1";
     }
 }
